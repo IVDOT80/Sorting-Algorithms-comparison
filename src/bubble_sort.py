ac3fe5ed7efbd8163fe787aefbfd4fe67055ef9c
IVDOT80 <mahdimahmoudnazari@gmail.com>
2025-10-28 18:54:16 +0330

    Adding bubble sorting algorithm

--- src/bubble_sort.py
+++ src/bubble_sort.py
@@ -1,1 +1,18 @@
+from typing import List
 
+def bubble_sort(list_numb: List[int],/) -> List[int]:
+    """
+    Sorts a list using Bubble Sort algorithm.
+
+    Idea: Compare neighbors, swap if wrong order. After each pass, the biggest element moves to the end.
+
+    :param list: The list to be sorted
+    :type list: List[int]
+    :return: The sorted list
+    :rtype: List[int]
+    """
+    for _ in range(len(list_numb)-1):    
+        for i in range(len(list_numb)-1):
+            if list_numb[i] > list_numb[i+1]:
+                list_numb[i+1],list_numb[i] = list_numb[i], list_numb[i+1]
+    return list_numb
