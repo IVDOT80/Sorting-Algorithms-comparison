4a88d103416e96ab177f40803fa541bc4bf5a406
IVDOT80 <mahdimahmoudnazari@gmail.com>
2025-11-01 16:52:21 +0330

    Adding measurement in utils

---
+++ utils/utils.py
@@ -0,0 +1,35 @@
+import time
+import sys
+from typing import Callable, List
+
+def measure_time(func: Callable,/, list: List[int]) -> float:
+    """
+    Measures time complexity of a function
+
+    :param func: The function to test
+    :type func: Callable
+    :param list: The list to pass to the function
+    :type list: List[int]
+    :return: The time taken by the function
+    :rtype: float
+    """ 
+    start_time = time.perf_counter()
+    func(list)
+    end_time = time.perf_counter()
+    return end_time - start_time
+
+def measure_space(func: Callable,/, list: List[int]) -> int:
+    """
+    Measures space complexity of a function
+
+    :param func: The function to test
+    :type func: Callable
+    :param list: The list to pass to the function
+    :type list: List[int]
+    :return: The memory used by the list
+    :rtype: int
+    """ 
+    start_memory = sys.getsizeof(list)
+    func(list)
+    end_memory = sys.getsizeof(list)
+    return end_memory - start_memory
